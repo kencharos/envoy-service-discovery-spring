e5d8dd821037dfc2669ba7110dec95044cb19cd1
kencharos <many.front@gmail.com>
2019-03-05 15:43:51 +0900

    add 3 empty spring projects

---
+++ backend1/src/main/kotlin/my/sdtest/backend1/Backend1Application.kt
@@ -0,0 +1,11 @@
+package my.sdtest.backend1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Backend1Application
+
+fun main(args: Array<String>) {
+    runApplication<Backend1Application>(*args)
+}
---
+++ backend2/src/test/java/my/sdtest/backend2/Backend2ApplicationTests.java
@@ -0,0 +1,16 @@
+package my.sdtest.backend2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Backend2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
